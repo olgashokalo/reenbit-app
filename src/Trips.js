cb993f7c1558e394125d47cb93d931d1107dc17b
Olga Shokalo <olha.shokalo@gmail.com>
2023-08-01 14:55:57 +0200

    Update structure

--- src/Trips.js
+++ src/Trips.js
@@ -6,28 +6,24 @@ export default function Trips() {
     <section className="grid grid-4-columns trips">
       <div className="trips--city">
         <ul>
-          <img></img>
           <li>Berlin</li>
           <li>Date</li>
         </ul>
       </div>
       <div className="trips--city">
         <ul>
-          <img></img>
           <li>Tokyo</li>
           <li>Date</li>
         </ul>
       </div>
       <div className="trips--city">
         <ul>
-          <img></img>
           <li>Barcelona</li>
           <li>Date</li>
         </ul>
       </div>
       <div className="trips--city">
         <ul>
-          <img></img>
           <li>+</li>
           <li>Add Trip</li>
         </ul>
